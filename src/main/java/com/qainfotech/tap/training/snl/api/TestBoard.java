0d94a281b18ab28460cbf50aa7b07230f18e3b5f
Srishti-qait <srishtiagarwal@qaifotech.com>
2017-06-30 14:19:46 +0530

    test cases made

--- src/main/java/com/qainfotech/tap/training/snl/api/TestBoard.java
+++
@@ -1,5 +0,0 @@
-package com.qainfotech.tap.training.snl.api;
-
-public class TestBoard {
-
-}
--- src/test/java/com/qainfotech/tap/training/snl/api/BoardTest.java
+++ src/test/java/com/qainfotech/tap/training/snl/api/BoardTest.java
@@ -9,96 +9,232 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class BoardTest {
 
 	Board boardTest;
+	Board boardTestReader;
 
-	@BeforeTest
-
+	@BeforeMethod
 	public void loadDB() throws FileNotFoundException, UnsupportedEncodingException, IOException, PlayerExistsException,
 			GameInProgressException, MaxPlayersReachedExeption {
 		boardTest = new Board();
 		boardTest.registerPlayer("srishti");
 		boardTest.registerPlayer("purvi");
 		boardTest.registerPlayer("aman");
-		
 
 	}
 
-	
-	@Test(expectedExceptions=PlayerExistsException.class)
-	public void acheckANameOfPlayerAlreadyExist() throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException
-	{
+	@Test
+	public void check_For_Constructor() throws IOException {
+		JSONObject data1 = boardTest.data;
+		Board board = new Board(boardTest.uuid);
+		JSONObject data2 = board.data;
+		assertThat(data1).isNotEqualTo(data2);
+
+	}
+
+	@Test(expectedExceptions = PlayerExistsException.class)
+	public void check_Name_Of_Player_Already_Exist() throws FileNotFoundException, UnsupportedEncodingException,
+			PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException {
 		boardTest.registerPlayer("srishti");
 	}
-@Test(expectedExceptions=MaxPlayersReachedExeption.class)
 
-public void bcheckMaximumNumberOfPlayer() throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException
-{
-	boardTest.registerPlayer("arpit");
-	boardTest.registerPlayer("nachi");
-}
+	@Test(expectedExceptions = MaxPlayersReachedExeption.class)
 
-@Test(expectedExceptions=NoUserWithSuchUUIDException.class)
-public void cNoUserWithSuchUUID() throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException,
-		GameInProgressException, MaxPlayersReachedExeption, IOException, NoUserWithSuchUUIDException, InvalidTurnException {
-           
-        boardTest.deletePlayer(this.boardTest.getUUID());
-        
- 
-}
-
-public void dtoCheckIfPlayerDeleted() throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException,
-GameInProgressException, MaxPlayersReachedExeption, IOException, NoUserWithSuchUUIDException, InvalidTurnException {
-   UUID uuid= (UUID)boardTest.getData().getJSONArray("players").getJSONObject(0).get("uuid");
-	   boardTest.deletePlayer(uuid);
-	
-assertThat(boardTest.getData().length()).isEqualTo(3);
-
-
-}
-/*@Test
-public void toCheckRollDice() throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException,
-GameInProgressException, MaxPlayersReachedExeption, IOException, NoUserWithSuchUUIDException, InvalidTurnException {
-	 UUID uuid= (UUID)boardTest.getData().getJSONArray("players").getJSONObject(1).get("uuid");
-	 
-	 boardTest.rollDice(uuid);
-	 
-}
-*/
-	// assertThat(boardTest.registerPlayer("purvi").length()).isEqualTo(3);
-
-	   @Test(expectedExceptions=GameInProgressException.class)
-	public void etoCheckGameAlraedyInProgress() throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException, InvalidTurnException, NoUserWithSuchUUIDException
-	{
-		   UUID uuid= (UUID)boardTest.getData().getJSONArray("players").getJSONObject(0).get("uuid");
-		   boardTest.deletePlayer(uuid);
-	
-	boardTest.rollDice((UUID) ((JSONObject) boardTest.getData().getJSONArray("players").get(0)).get("uuid"));
-	boardTest.registerPlayer("Anvit");
+	public void check_Maximum_Number_Of_Player() throws FileNotFoundException, UnsupportedEncodingException,
+			PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException {
+		boardTest.registerPlayer("arpit");
+		boardTest.registerPlayer("nachi");
 	}
-	
 
-	   @Test(expectedExceptions=InvalidTurnException.class)
-	public void ftoCheckInvalidTurn() throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException, InvalidTurnException, NoUserWithSuchUUIDException
-	{
-		   
-	boardTest.rollDice((UUID) ((JSONObject) boardTest.getData().getJSONArray("players").get(2)).get("uuid"));
-	
-	
+	@Test(expectedExceptions = NoUserWithSuchUUIDException.class)
+	public void no_UserWithSuchUUID()
+			throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
+			MaxPlayersReachedExeption, IOException, NoUserWithSuchUUIDException, InvalidTurnException {
+
+		boardTest.deletePlayer(this.boardTest.getUUID());
+
 	}
-	   public void ftoCheckPositionTurn() throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException, InvalidTurnException, NoUserWithSuchUUIDException
-		{
-			   
+
+	public void to_Check_If_A_Player_Deleted()
+			throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
+			MaxPlayersReachedExeption, IOException, NoUserWithSuchUUIDException, InvalidTurnException {
+		UUID uuid = (UUID) boardTest.getData().getJSONArray("players").getJSONObject(0).get("uuid");
+		boardTest.deletePlayer(uuid);
+
+		assertThat(boardTest.getData().length()).isEqualTo(2);
+
+	}
+
+	@Test(expectedExceptions = GameInProgressException.class)
+	public void to_Check_If_Game_Alraedy_In_Progress()
+			throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
+			MaxPlayersReachedExeption, IOException, InvalidTurnException, NoUserWithSuchUUIDException {
+		// UUID uuid = (UUID)
+		// boardTest.getData().getJSONArray("players").getJSONObject(0).get("uuid");
+		// boardTest.deletePlayer(uuid);
+
+		boardTest.rollDice((UUID) ((JSONObject) boardTest.getData().getJSONArray("players").get(0)).get("uuid"));
+		boardTest.registerPlayer("Anvit");
+	}
+
+	@Test(expectedExceptions = InvalidTurnException.class)
+	public void to_Check_Invalid_Turn()
+			throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
+			MaxPlayersReachedExeption, IOException, InvalidTurnException, NoUserWithSuchUUIDException {
+
+		boardTest.rollDice((UUID) ((JSONObject) boardTest.getData().getJSONArray("players").get(2)).get("uuid"));
+
+	}
+
+	@Test
+	public void to_Check_Position_Is_Equal_Or_Not()
+			throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
+			MaxPlayersReachedExeption, IOException, InvalidTurnException, NoUserWithSuchUUIDException {
+		Object initial = ((JSONObject) boardTest.getData().getJSONArray("players").get(0)).getInt("position");
+		UUID uuid = (UUID) ((JSONObject) boardTest.getData().getJSONArray("players").get(0)).get("uuid");
+
+		Object dice = boardTest.rollDice(uuid).get("dice");
+		Object finalPosition = ((JSONObject) boardTest.getData().getJSONArray("players").get(0)).getInt("position");
+		Object rolledDice = boardTest.getData().getJSONArray("steps").getJSONObject((int) initial + (int) dice)
+				.get("target");
+
+		assertThat(rolledDice).isEqualTo(finalPosition);
+		int h = (int) finalPosition;
+		((JSONObject) boardTest.getData().getJSONArray("steps").get(h)).getInt("type");
+
+	}
+
+	@Test
+	public void to_Check_Position_Turn()
+			throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
+			MaxPlayersReachedExeption, IOException, InvalidTurnException, NoUserWithSuchUUIDException {
+		boardTest.rollDice((UUID) ((JSONObject) boardTest.getData().getJSONArray("players").get(0)).get("uuid"));
 		boardTest.rollDice((UUID) ((JSONObject) boardTest.getData().getJSONArray("players").get(1)).get("uuid"));
-		
-		
-		
-		}
-	
-	
+		boardTest.rollDice((UUID) ((JSONObject) boardTest.getData().getJSONArray("players").get(2)).get("uuid"));
+		assertThat(boardTest.getData().get("turn")).isEqualTo(0);
+	}
 
+	@Test
+	public void to_Check_Position_Move()
+			throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
+			MaxPlayersReachedExeption, IOException, InvalidTurnException, NoUserWithSuchUUIDException {
+		Object initial = ((JSONObject) boardTest.getData().getJSONArray("players").get(0)).getInt("position");
+		UUID uuid = (UUID) ((JSONObject) boardTest.getData().getJSONArray("players").get(0)).get("uuid");
+		Object obj = boardTest.rollDice(uuid);
+		Object msg = ((JSONObject) obj).get("message");
+		Object dice = ((JSONObject) obj).get("dice");
+		Object finalPosition = boardTest.getData().getJSONArray("steps").getJSONObject((int) initial + (int) dice)
+				.get("target");
+		int h = (int) initial + (int) dice;
+		int a = ((JSONObject) boardTest.getData().getJSONArray("steps").get(h)).getInt("type");
+		if (a == 0) {
+			assertThat(msg.toString()).isEqualTo("Player moved to " + finalPosition);
+		}
+
+	}
+
+	@Test
+	public void to_Check_New_Position()
+			throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
+			MaxPlayersReachedExeption, IOException, InvalidTurnException, NoUserWithSuchUUIDException {
+		((JSONObject) boardTest.getData().getJSONArray("players").get(1)).put("position", 100);
+		Object d = ((JSONObject) boardTest.getData().getJSONArray("players").get(0)).getInt("position");
+		UUID uuid = (UUID) ((JSONObject) boardTest.getData().getJSONArray("players").get(0)).get("uuid");
+		Object obj = boardTest.rollDice(uuid);
+		Object msg = ((JSONObject) obj).get("message");
+		if ((int) d >= 100)
+			assertThat(msg.toString()).isEqualTo("Incorrect roll of dice. Player did not move");
+
+	}
+
+	@Test
+	public void to_Check_Position_UUID()
+			throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
+			MaxPlayersReachedExeption, IOException, InvalidTurnException, NoUserWithSuchUUIDException {
+		UUID firstPlayer = (UUID) boardTest.getData().getJSONArray("players").getJSONObject(0).get("uuid");
+
+		assertThat(boardTest.toString()).isNotEqualTo(firstPlayer.toString());
+	}
+
+	@Test
+	public void To_Check_Snake() throws FileNotFoundException, UnsupportedEncodingException, IOException,
+			PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, InvalidTurnException {
+		Board board = new Board();
+		board.registerPlayer("s1");
+		board.registerPlayer("s2");
+		board.registerPlayer("s3");
+		board.registerPlayer("s4");
+		JSONArray steps = new JSONArray();
+		steps = board.getData().getJSONArray("steps");
+		JSONObject step = new JSONObject();
+		for (int j = 1; j < 7; j++) {
+			step = steps.getJSONObject(j);
+			step.put("type", 1);
+			step.put("target", j - 1);
+		}
+		JSONObject data = board.getData();
+		JSONArray playerArray = data.getJSONArray("players");
+		for (int i = 0; i < 4; i++) {
+			int turn = (int) data.get("turn");
+			JSONObject singlePlayer = playerArray.getJSONObject(turn);
+			UUID singlePlayerUUID = (UUID) singlePlayer.get("uuid");
+			int currentPosition = (int) singlePlayer.get("position");
+			JSONObject response = board.rollDice(singlePlayerUUID);
+			int newPosition = (int) singlePlayer.get("position");
+			assertThat(data.getJSONArray("steps").getJSONObject((int) response.get("dice")).get("target"))
+					.isEqualTo(newPosition);
+			Object dice = response.get("dice");
+			Object msg = response.get("message");
+			int position = (int) currentPosition + (int) dice;
+			
+			int type = ((JSONObject) board.getData().getJSONArray("steps").get(position)).getInt("type");
+			if (type == 1) {
+				
+				assertThat(msg.toString()).isEqualTo("Player was bit by a snake, moved back to " + newPosition);
+			}
+		}
+	}
+
+	@Test
+	public void To_Check_ladder() throws FileNotFoundException, UnsupportedEncodingException, IOException,
+			PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, InvalidTurnException {
+		Board board = new Board();
+		board.registerPlayer("s1");
+		board.registerPlayer("s2");
+		board.registerPlayer("s3");
+		board.registerPlayer("s4");
+		JSONArray steps = new JSONArray();
+		steps = board.getData().getJSONArray("steps");
+		JSONObject step = new JSONObject();
+		for (int j = 1; j < 7; j++) {
+			step = steps.getJSONObject(j);
+			step.put("type", 1);
+			step.put("target", j * 10);
+		}
+		JSONObject data = board.getData();
+		JSONArray playerArray = data.getJSONArray("players");
+		for (int i = 0; i < 4; i++) {
+			int turn = (int) data.get("turn");
+			JSONObject singlePlayer = playerArray.getJSONObject(turn);
+			UUID singlePlayerUUID = (UUID) singlePlayer.get("uuid");
+			int currentPosition = (int) singlePlayer.get("position");
+			JSONObject response = board.rollDice(singlePlayerUUID);
+			int newPosition = (int) singlePlayer.get("position");
+			assertThat(data.getJSONArray("steps").getJSONObject((int) response.get("dice")).get("target"))
+					.isEqualTo(newPosition);
+			Object dice = response.get("dice");
+			Object msg = response.get("message");
+			int position = (int) currentPosition + (int) dice;
+		;
+			int type = ((JSONObject) board.getData().getJSONArray("steps").get(position)).getInt("type");
+			if (type == 2) {
+				
+				assertThat(msg.toString()).isEqualTo("Player climbed a ladder, moved to " + newPosition);
+			}
+		}
+	}
 }
